da7215e2d46b353ba1978804bfc0385e93f59944
PeterKhoudary <peter.m.khoudary@example.com>
2025-10-04 01:10:26 -0400

    initial draft thanks michael

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,63 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="contentWrapper">
+    <div class="mainContent">
+      <div class="headerContent">
+        <h1>Peter Khoudary</h1>
+        <div class="links">
+          <h1><a href="https://www.youtube.com/@peterkhoudary" target="_blank">YT</a></h1>
+          <h1><a href="https://www.instagram.com/peterkhoudary/" target="_blank">IG</a></h1>
+        </div>
+      </div>
+      <img src="../assets/mountains.jpg" class="mainImage" />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.contentWrapper {
+  display: flex;
+  justify-content: center;
+}
+.mainContent {
+  margin: 10rem;
+  display: flex;
+  flex-direction: column;
+  width: min(60%, 1200px);
+}
+
+.headerContent {
+  display: flex;
+  justify-content: space-between;
+}
+
+.links {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 20px;
+}
+
+.mainImage {
+  border-style: solid;
+  border-width: 4px;
+  border-color: black;
+  width: 100%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+  transition: color 0.2s ease-in-out;
+}
+
+a:hover {
+  color: orangered;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
